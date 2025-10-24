dae1abb387e5e80edaea1de842d4d2af748b0d92
Spilsed <93678295+Spilsed@users.noreply.github.com>
2025-10-24 15:31:49 -0400

    Add basic blog page

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Card from '../Card.svelte';
+</script>
+
+<div class="p-4">
+	<div class="mr-auto ml-auto max-w-200">
+		<div class="mb-4 grid grid-cols-3 grid-rows-1 items-center border-b-2 pb-2">
+			<a class="text-xl" href="/">Titan Spellos</a>
+			<h1 class="text-center text-4xl font-bold">Blog</h1>
+			<a class="text-right text-xl" href="/">Home</a>
+		</div>
+	</div>
+</div>
