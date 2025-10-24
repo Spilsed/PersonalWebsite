442cd52b4145620b12ec24494e4f067d52315570
Spilsed <93678295+Spilsed@users.noreply.github.com>
2025-10-24 15:33:37 -0400

    Fully remove favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
 	import '../app.css';
-	// import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<!-- <link rel="icon" href={favicon} /> -->
-</svelte:head>
 
 <div class="h-screen w-screen bg-surface-950 text-white">
 	{@render children?.()}
